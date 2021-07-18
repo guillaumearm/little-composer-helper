8eb212faf2ad9d78fce46ac89a097b5aa0c5e34c
Guillaume ARM <trapcodien@gmail.com>
2021-07-18 15:30:40 +0200

    feat: improve ChromaticCounter

--- src/containers/ChromaticCounter/index.tsx
+++ src/containers/ChromaticCounter/index.tsx
@@ -1,112 +1,135 @@
-import type { Component } from 'solid-js';
-import { Note } from '../../domain/notes';
+import { Accessor, Component, createSignal, For } from 'solid-js';
+import { changeBase, CHROMATIC_SCALE, Note } from '../../domain/notes';
 
-type KeyProps = { note: Note; text?: string; extended?: boolean };
+const CHROMATIC_SCALE_BASE_C = changeBase(CHROMATIC_SCALE, 'C');
 
-const WhiteKey: Component<KeyProps> = (props) => {
+type Callback = () => void;
+
+type NoteCounterProps = {
+  note: Note;
+  sharp: boolean;
+  count: Accessor<number>;
+  plus: Callback;
+  minus: Callback;
+  reset: Callback;
+};
+
+const createNoteCounter = (note: Note): NoteCounterProps => {
+  const [getCount, setCount] = createSignal(0);
+
+  const plus = () => {
+    setCount((x) => x + 1);
+  };
+  const minus = () => setCount((x) => Math.max(0, x - 1));
+  const reset = () => setCount(0);
+
+  return {
+    note,
+    sharp: note.length > 1,
+    count: getCount,
+    plus,
+    minus,
+    reset,
+  };
+};
+
+const NoteCounter: Component<NoteCounterProps> = (props) => {
+  const buttonStyle = {
+    padding: '2px',
+    margin: '2px',
+    // width: '20px',
+  };
+
   return (
-    <div
-      style={{
-        'border-top': '1px solid black',
-        'padding-top': props.extended ? '18px' : '10px',
-        'padding-left': '10px',
-        'padding-bottom': props.extended ? '18px' : '0px',
-        'background-color': 'white',
-      }}
-    >
-      <span style={{ position: 'absolute', 'margin-left': '220px' }}>
-        <button style={{ 'margin-left': '10px', 'border-radius': '0px' }}>
-          +
-        </button>
-        <button style={{ 'margin-left': '10px', 'border-radius': '0px' }}>
-          -
-        </button>
-      </span>
-      <span
+    <div>
+      <div
         style={{
-          'border-radius': '10px',
-          padding: '4px',
-          'background-color': 'lightgray',
+          display: 'inline-block',
+          'padding-top': '8px',
+          'background-color': props.sharp ? 'black' : 'white',
+          color: props.sharp ? 'white' : 'black',
+          width: props.sharp ? '35px' : '60px',
+          'text-align': props.sharp ? 'inherit' : 'center',
+          'padding-left': props.sharp ? '5px' : '0px',
+          'margin-left': props.sharp ? '20px' : '0px',
         }}
       >
         {props.note}
-      </span>
-      <span>{props.text ? ` - ${props.text}` : ''}</span>
-    </div>
-  );
-};
-
-const BlackKey: Component<KeyProps> = (props) => {
-  return (
-    <div style={{ 'background-color': 'white' }}>
-      <div
+      </div>
+      <span
         style={{
-          'padding-top': '5px',
-          'padding-bottom': '5px',
-          'padding-left': '10px',
-          'margin-left': '80px',
-          'background-color': 'black',
+          'background-color': 'gray',
           color: 'white',
+          'padding-top': '8px',
+          'padding-left': '10px',
+          'padding-right': '10px',
         }}
       >
+        <button onClick={props.plus} style={buttonStyle}>
+          +
+        </button>
+        <button onClick={props.minus} style={buttonStyle}>
+          -
+        </button>
+        <button onClick={props.reset} style={buttonStyle}>
+          Reset
+        </button>
         <span
           style={{
-            'border-radius': '10px',
-            padding: '4px',
-            'background-color': 'gray',
+            'margin-left': '10px',
+            'background-color': 'lightgray',
+            color: 'black',
           }}
         >
-          {props.note}
+          {props.count()}
         </span>
-        <span
-          style={{
-            position: 'absolute',
-            'margin-left': '110px',
-          }}
-        >
-          <button style={{ 'border-radius': '0px', 'margin-left': '10px' }}>
-            +
-          </button>
-          <button style={{ 'border-radius': '0px', 'margin-left': '10px' }}>
-            -
-          </button>
-        </span>
-        <span>{props.text ? ` - ${props.text}` : ''}</span>
-      </div>
+      </span>
     </div>
   );
 };
 
-const ChromaticLayout: Component = () => {
+const ChromaticLayout: Component<{ noteCounters: NoteCounterProps[] }> = (
+  props,
+) => {
   return (
-    <>
-      <WhiteKey note={'C'} />
-      <BlackKey note={'C#'} />
-      <WhiteKey note={'D'} />
-      <BlackKey note={'D#'} />
-      <WhiteKey note={'E'} extended />
-      <WhiteKey note={'F'} />
-      <BlackKey note={'F#'} />
-      <WhiteKey note={'G'} />
-      <BlackKey note={'G#'} />
-      <WhiteKey note={'A'} />
-      <BlackKey note={'A#'} />
-      <WhiteKey note={'B'} extended />
-    </>
+    <div
+      style={{
+        'background-color': 'lightgray',
+        padding: '10px',
+      }}
+    >
+      <For each={props.noteCounters}>
+        {(noteCounterProps) => {
+          return <NoteCounter {...noteCounterProps} />;
+        }}
+      </For>
+    </div>
   );
 };
 
 export const ChromaticCounter: Component = () => {
+  const noteCounters = CHROMATIC_SCALE_BASE_C.map((note) =>
+    createNoteCounter(note),
+  );
+
+  const onResetAll = () => {
+    noteCounters.forEach((noteCounter) => {
+      noteCounter.reset();
+    });
+  };
+
   return (
     <div
       style={{
-        margin: 'auto',
-        'background-color': 'lightgray',
+        // margin: 'auto',
+        'margin-left': '42px',
+        'background-color': 'gray',
         width: '300px',
-        padding: '10px',
+        padding: '2px',
       }}
     >
-      <ChromaticLayout />
+      <button onClick={onResetAll}>RESET ALL</button>
+      <ChromaticLayout noteCounters={noteCounters} />
     </div>
   );
 };
