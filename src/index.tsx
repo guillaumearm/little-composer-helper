4044dd0544b8ab12e0f0cce46abe17733070ce4a
Guillaume ARM <trapcodien@gmail.com>
2021-08-03 16:27:30 +0200

    build: add tsc npm script

--- src/index.tsx
+++ src/index.tsx
@@ -3,4 +3,10 @@ import { render } from 'solid-js/web';
 import './index.css';
 import App from './App';
 
-render(() => <App />, document.getElementById('root'));
+const rootElement = document.getElementById('root');
+
+if (!rootElement) {
+  throw new Error('Fatal: no div #root found in the DOM');
+}
+
+render(() => <App />, rootElement);
